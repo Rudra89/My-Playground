c407def4113d95041fade74d29faa3c810e1e4fb
Rudra <rudrapathak28@gmail.com>
2025-08-15 20:47:52 +0530

    Demo

---
+++ todo_gui/functions.py
@@ -0,0 +1,17 @@
+FILEPATH = "todo_gui/todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Read a text file and return the list of to-do
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_list, filepath=FILEPATH):
+    """
+    Write a to-do items list in the text file
+    """
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_list)
